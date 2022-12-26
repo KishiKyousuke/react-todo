94111d1949d5ebc244d2ac1d2e1dab85797fef2a
KishiKyosuke（Kyo18） <carp011808pg@icloud.com>
2022-12-26 23:45:33 +0000

    lec.52 HTML作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,35 @@ import "./styles.css";
 
 export const App = () => {
   return (
-    <div></div>  
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>あああああ</li>
+            <button>完了</button>
+            <button>削除</button>  
+          </div>
+          <div>
+            <li>いいいいい</li>
+            <button>完了</button>
+            <button>削除</button>  
+          </div>
+        </ul>  
+      </div>  
+      <div>
+      <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>あああああ</li>
+            <button>戻す</button>
+          </div>
+        </ul>  
+      </div>
+    </>
   )
 }
